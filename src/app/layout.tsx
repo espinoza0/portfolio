4838bfe1d8726b3233b9615052ee3b142de36b0a
espinoza0 <gabriel.bascope005@gmail.com>
2025-04-17 19:10:27 +0200

    hero seccion

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,22 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Onest } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
+
+const onest = Onest({
+  variable: "--font-onest",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,12 +29,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="es" className={onest.variable}>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`antialiased bg-[#070B0C]`}
       >
-        <div className="container mx-auto">
-          <header></header>
+        <div className="max-w-[70rem] mx-auto">
+          <header className="flex items-center justify-center">
+            <Header/>
+          </header>
           <main>{children}</main>
           <footer></footer>
         </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import { Button } from "@/components/ui/button";
+"use client";
+
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-   <>
-    <h1 className="text-pine-green">hola</h1>
-    <Button>asdfasdf</Button>
-   </>
+    <>
+      <Hero />
+    </>
   );
 }
